8d5257fc9cc3681512c3ffe9822e8670f0ae19d7
Cristopher Fontes <cris.fontstudy@gmail.com>
2021-12-05 21:43:45 -0300

    Creamos el proyecto y los componentes necesarios.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistroComponent } from './Components/registro/registro.component';
+import { LoginComponent } from './Components/login/login.component';
+import { ListadoComponent } from './Components/listado/listado.component';
+import { InfoComponent } from './Components/info/info.component';
+import { CarritoComponent } from './Components/carrito/carrito.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistroComponent,
+    LoginComponent,
+    ListadoComponent,
+    InfoComponent,
+    CarritoComponent
   ],
   imports: [
     BrowserModule,
